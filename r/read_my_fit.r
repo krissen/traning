6aa8dcf4a868f28a01faef9cf28a9cf451f4ba9a
Kristian Niemi <kristian.niemi@gmail.com>
2020-05-14 21:37:38 +0200

    tvungen ändra bibliotek. Läsa tcx ist f fit

--- r/read_my_fit.r
+++ r/read_my_fit.r
@@ -1,50 +1,15 @@
 #!/usr/local/bin/R
 
-library(fitdc)
+# library(fitdc)
+library(trackr)
 library(lubridate)
+suppressMessages(suppressWarnings(library(tidyverse)))
 
-afile <- "../kristian/filer/fit/2015/2015-01-26_08-54-50-80-12991.fit"
-
-
-is_record <- function(mesg) mesg$name == "record"
-
-format_record <- function(record) {
-  out <- record$fields
-  names(out) <- paste(names(out), record$units, sep = ".")
-  out
-}
-
-merge_lists <- function(ls_part, ls_full) {
-  extra <- setdiff(names(ls_full), names(ls_part))
-  append(ls_part, ls_full[extra])[names(ls_full)]  # order as well
-}
-
-fetch_run <- function(fitfile) {
-  data_mesgs <- read_fit(fitfile)
-
-  records <- Filter(is_record, data_mesgs)
-
-  records <- lapply(records, format_record)
-
-  ## Some records have missing fields:
-
-  colnames_full <- names(records[[which.max(lengths(records))]])
-
-  empty <- setNames(
-                    as.list(rep(NA, length(colnames_full))),
-                    colnames_full)
-
-  records <- lapply(records, merge_lists, empty)
-  records <- data.frame(
-                        do.call(rbind, records))
-
-  return(records)
-}
 
 fetch_run_overview <- function(arun) {
   # arun <- run_details
   last_entry <- length(arun$distance.m)
-  distance <- arun$distance.m[[my_last_entry]]
+  distance <- arun$distance.m[[last_entry]]
   hr_mean <- mean(as.numeric(arun$heart_rate.bpm), na.rm = TRUE)
   hr_max <- max(as.numeric(arun$heart_rate.bpm), na.rm = TRUE)
   
@@ -69,7 +34,28 @@ fetch_run_overview <- function(arun) {
   return(overview)
 }
 
-run_details <- fetch_run(afile)
+files <- list.files(path="../kristian/filer/tcx",
+                    recursive = TRUE,
+                    pattern="*.fit",
+                    ignore.case = TRUE,
+                    full.names=TRUE)
+
+files %>%
+  .[!grepl("fit/0000", .)] -> files
+
+df <- data.frame()
+
+for ( file in files ) {
+  tryCatch({
+    run_details <- fetch_run(file)
+  run_overview <- fetch_run_overview(run_details)
+  df <- rbind(df, run_overview)
+  }, error = function(e){})
+}
+
+test <- readTCX(file, timezone = "", speedunit = "m_per_s",
+        distanceunit = "m")
+run_details <- fetch_run(file)
 
 run_overview <- fetch_run_overview(run_details)
 
